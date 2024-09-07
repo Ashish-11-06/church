2582a4cad0f2664f07d9fcd49cbdd0f68cadd236
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-09-07 16:51:37 +0530

    settled the image

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../Style/Events.css'; // Adjust the path as needed
 
+import groto from '../Assets/groto.jpeg';
+
 const Events = () => (
-  <div className="container mt-2 custom-background p-4 rounded">
-    <h1 className="display-4 text-center mb-4">हरेगाव यात्रा महोत्सव</h1>
-    <h4 className="text-muted text-center mb-3">The Feast Of Immaculate Virgin Mary</h4>
-    <p className="text-center">पवित्र निष्कलंक मारियामतेचा सण</p>
-    <h3 className="text-primary text-center">14 Sept 2024</h3>
+  <div className="position-relative">
+    <img
+      src={groto}
+      alt="Groto"
+      className="img-fluid"
+    />
+    <div className="text-overlay">
+      <h1 className="event-title">हरेगाव यात्रा महोत्सव</h1>
+      <h4 className="event-subtitle">The Feast Of Immaculate Virgin Mary</h4>
+      <p className="event-description">पवित्र निष्कलंक मारियामतेचा सण</p>
+      <h3 className="event-date">14 Sept 2024</h3>
+    </div>
   </div>
 );
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,32 +1,29 @@
-import '../Style/Home.css'
+import '../Style/Home.css';
 import React from 'react';
-import Events from './Events'
+import Events from './Events';
 import Contact from './Contact';
 
-import churchImage from '../Assets/church3.png'
-import groto from '../Assets/groto.jpeg'
+import churchImage from '../Assets/church3.png';
 
 const HomePage = () => (
   <main className='home'>
-     <div className=" text-white p-2 d-flex align-items-center">
-     <img
-      src={groto}
-      alt="Groto"
-      style={{ maxHeight: '100px', width: 'auto', marginRight: '15px' }} // Adjust maxHeight and marginRight as needed
-      />
-      <div className=" text-white  align-items-center">
-      <h2>st. Teresa Church Harigaon</h2>
-      <p>जो माझ्या पुत्राचा सन्मान करील, त्यांच्या ईच्छा मी पूर्ण करील. </p>
-      </div>
-   </div>
-   
-   <img
-      src={churchImage}
+    <div className="text-white p-2 d-flex align-items-center">
+      <img
+       src={churchImage}
       alt="Church"
-      style={{ maxHeight: '100px', width: 'auto', marginRight: '15px' }} // Adjust maxHeight and marginRight as needed
+        
+        style={{ maxHeight: '100px', width: 'auto', marginRight: '15px' }} // Adjust as needed
       />
-  <Events />
-  <Contact />
+      <div className="text-white align-items-center">
+        <h2>St. Teresa Church Harigaon</h2>
+        <p>जो माझ्या पुत्राचा सन्मान करील, त्यांच्या ईच्छा मी पूर्ण करील.</p>
+      </div>
+    </div>
+
+  
+    
+    <Events />
+    <Contact />
   </main>
 );
 
