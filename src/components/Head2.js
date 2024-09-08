3ee9590dbd4ce86748fcac123e4a64ba045fd407
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-09-08 16:51:59 +0530

    near to final step

--- src/components/Head2.js
+++ src/components/Head2.js
@@ -6,15 +6,17 @@ const QuoteSection = () => {
   return (
     <div className="quote-section">
      
-      {/* <blockquote className="quote-text">
+     
+<blockquote className="quote-text">
+      "Holy Mary, Mother of God,
+pray for us sinners, now and at the hour of our death. Amen."      </blockquote>
+
+ {/* <blockquote className="quote-text">
       "Hail Mary, full of grace, the Lord is with thee;
 blessed art thou among women,
 and blessed is the fruit of thy womb, Jesus."      </blockquote> */}
 <img src={groto} alt="Saint" className="saint-image" />
 
-<blockquote className="quote-text">
-      "Holy Mary, Mother of God,
-pray for us sinners, now and at the hour of our death. Amen."      </blockquote>
     </div>
   );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,22 +7,27 @@ import Head2 from '../components/Head2';
 import Social from '../components/Social';
 
 const HomePage = () => (
-  <main className='home'>
+  <main className='home align-items-center'>
     
     <div className="marquee">
       <p>Festival is on 14 September 2024.    सण १४ सप्टेंबर २०२४ रोजी आहे</p>
     </div>
-    <div className="text-white p-2 d-flex align-items-center">
-      <img
-        src={churchImage}
-        alt="Church"
-        style={{ maxHeight: '100px', width: 'auto', marginRight: '15px' }}
-      />
-      <div className="text-white align-items-center">
-        <h2>St. Teresa Church Harigaon</h2>
-        <p>"जे माझा सन्मान करतील, त्यांच्या ईच्छा मी पूर्ण करीन."</p>
-      </div>
-    </div>
+   <section className='Head'>
+
+   <div className="text-white p-2 d-flex align-items-center justify-content-center">
+  <img
+    src={churchImage}
+    alt="Church"
+    style={{ maxHeight: '100px', width: 'auto', marginRight: '15px' }}
+  />
+  <div className="text-white d-flex flex-column align-items-center">
+    <h2 className="mb-2">St. Teresa Church Harigaon</h2>
+    <p className="text-center">"जे माझा सन्मान करतील, त्यांच्या ईच्छा मी पूर्ण करीन."</p>
+  </div>
+</div>
+
+
+   </section>
 
     <Head2 />
 
