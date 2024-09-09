d4e7149b4ed9f42194dddc1640061e59b4e1b69b
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-09-09 23:27:40 +0530

    contact added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../Style/Footer.css'
 const Footer = () => (
   <footer className="footer text-black text-center p-1">
     <p>&copy; 2024 Saint Teresa Church Harigaon. All rights reserved.</p>
-    <p className="small-text">the site is designed and developed by @Ashish Bhosale & team</p>
+    <p className="small-text">the site is designed and developed by @Ashish Bhosale helpline:9075943766</p>
   </footer>
 );
 
